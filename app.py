2f96dc5d57a2415e8def3b92280be35071011481
Fabiana Fernández <109298175+FabSignal@users.noreply.github.com>
2025-04-07 13:07:02 -0300

    archivos

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from streamlit_timeline import timeline
+import json
+
+# use full page width
+st.set_page_config(page_title="Timeline Example", layout="wide")
+
+# load data
+with open('linea_temp.json', "r") as f:
+    data = f.read()
+
+# render timeline
+timeline(data, height=800)
